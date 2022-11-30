d77d8ff9fc7cd6885a03e49d8deb16b1865b895c
Ali Jamalian <alijamalian@gmail.com>
2022-11-30 09:15:45 +0100

    add auth Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { RouterProvider } from 'react-router-dom'
+import { router } from 'lib/routers'
 
 export default function App() {
 	return (
 		<ChakraProvider>
+			<RouterProvider router={router} />
 			<h1>Test</h1>
 		</ChakraProvider>
 	)
--- src/lib/routers.js
+++ src/lib/routers.js
@@ -1,1 +1,22 @@
-export const router = {}
+import Login from 'components/auth/Login'
+import Register from 'components/auth/Register'
+import { createBrowserRouter } from 'react-router-dom'
+
+export const ROOT = '/'
+export const LOGIN = '/login'
+export const REGISTER = '/register'
+
+export const router = createBrowserRouter([
+	{
+		path: ROOT,
+		element: 'Public Root',
+	},
+	{
+		path: LOGIN,
+		element: <Login />,
+	},
+	{
+		path: REGISTER,
+		element: <Register />,
+	},
+])
